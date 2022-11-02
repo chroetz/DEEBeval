9425b6c3ed9b1faba6ddac8a27ec350db61ca701
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-11-02 22:33:56 +0100

    * runEval() * getNew()

--- R/evaluate.R
+++ R/evaluate.R
@@ -1,4 +1,11 @@
-evalMetaAndWriteToFile <- function(meta, outPath, method, verbose=TRUE) {
+evalMetaAndWriteToFile <- function(
+    meta,
+    outPath,
+    method,
+    createPlots = TRUE,
+    scoreFilter = NULL,
+    verbose=TRUE
+) {
 
   plotsPath <- file.path(outPath, "plots")
   if (!dir.exists(plotsPath)) dir.create(plotsPath, recursive=TRUE)
@@ -7,6 +14,8 @@ evalMetaAndWriteToFile <- function(meta, outPath, method, verbose=TRUE) {
   tbl <- evalTbl(
     meta,
     infoList = list(method = method, plotsPath = plotsPath),
+    createPlots = createPlots,
+    scoreFilter = scoreFilter,
     verbose = verbose)
 
   scoreTbl <-
@@ -16,13 +25,23 @@ evalMetaAndWriteToFile <- function(meta, outPath, method, verbose=TRUE) {
   scoresOutFiles <- sapply(
     unique(tbl$taskNr),
     \(tnr) {
-      taskscoreTbl <-
+      taskScoreTbl <-
         scoreTbl |>
         filter(.data$taskNr == tnr) |>
         tidyr::unnest_wider(.data$scores) |>
         relocate(.data$method, ends_with("Nr"))
       scoresOutFile <- file.path(outPath, DEEBpath::evalDataFile(method = method, taskNr = tnr))
-      readr::write_csv(taskscoreTbl, scoresOutFile)
+      if (file.exists(scoresOutFile)) {
+        oldScores <- readr::read_csv(scoresOutFile, col_types = cols())
+        retainedScores <- dplyr::semi_join(
+          oldScores,
+          taskScoreTbl,
+          by = c("method", "truthNr", "obsNr", "taskNr"))
+        taskScoreTbl <- bind_rows(
+          taskScoreTbl,
+          retainedScores)
+      }
+      readr::write_csv(taskScoreTbl, scoresOutFile)
       scoresOutFile
     })
 
@@ -34,17 +53,32 @@ evalMetaAndWriteToFile <- function(meta, outPath, method, verbose=TRUE) {
 }
 
 
-evalTbl <- function(meta, infoList = list(), verbose=TRUE) {
+evalTbl <- function(
+    meta,
+    infoList = list(),
+    createPlots = TRUE,
+    scoreFilter = NULL,
+    verbose = TRUE
+) {
 
   meta$scores <- lapply(
     seq_len(nrow(meta)),
-    \(i) evalOne(c(meta[i,], infoList), verbose))
+    \(i) evalOne(
+      c(meta[i,], infoList),
+      createPlots = createPlots,
+      scoreFilter = scoreFilter,
+      verbose = verbose))
 
   return(meta)
 }
 
 
-evalOne <- function(infoList, verbose=TRUE) {
+evalOne <- function(
+    infoList,
+    createPlots = TRUE,
+    scoreFilter = NULL,
+    verbose = TRUE
+) {
 
   infoList <- as.list(infoList)
   info <- new.env(parent=emptyenv())
@@ -56,22 +90,32 @@ evalOne <- function(infoList, verbose=TRUE) {
   info$title <- paste0(info$method, ", Task", info$taskNr, ", Truth", info$truthNr, ", Obs", info$obsNr)
   if (verbose) cat(info$title, "\n")
 
-  scoreFuns <- lapply(info$task$scoreList$list, buildScore, verbose=verbose)
-  names(scoreFuns) <- sapply(info$task$scoreList$list, \(x) x$name)
+  scoreNames <- sapply(info$task$scoreList$list, \(x) x$name)
+  if (is.null(scoreFilter)) {
+    selScores <- TRUE
+  } else {
+    selScores <- scoreNames %in% scoreFilter
+  }
+  scoreList <- info$task$scoreList$list[selScores]
+  scoreFuns <- lapply(scoreList, buildScore, verbose=verbose)
+  names(scoreFuns) <- scoreNames[selScores]
 
+  # calculate scores
   scores <- lapply(scoreFuns, do.call, args = list(info = info))
 
-  plots <- createTruthEstiTaskPlots(info)
-  for (nm in names(plots)) {
-    plt <- plots[[nm]]
-    fileName <- DEEBpath::parenthesisFileName(
-      truth = info$truthNr,
-      obs = info$obsNr,
-      task = info$taskNr,
-      method = info$method,
-      plot = nm,
-      .ending = "png")
-    ggsave(file.path(info$plotsPath, fileName), plt, width = 3, height = 3)
+  if (createPlots) {
+    plots <- createTruthEstiTaskPlots(info)
+    for (nm in names(plots)) {
+      plt <- plots[[nm]]
+      fileName <- DEEBpath::parenthesisFileName(
+        truth = info$truthNr,
+        obs = info$obsNr,
+        task = info$taskNr,
+        method = info$method,
+        plot = nm,
+        .ending = "png")
+      ggsave(file.path(info$plotsPath, fileName), plt, width = 3, height = 3)
+    }
   }
 
   return(scores)
