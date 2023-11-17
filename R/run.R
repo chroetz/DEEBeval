0d6b00e4665d78803f3950fd1076402b9460be7d
Christof Schoetz <cschoetz@pik-potsdam.de>
2023-11-17 12:03:00 +0100

    remove example, allow error in score doc

--- R/run.R
+++ R/run.R
@@ -2,7 +2,6 @@
 runEval <- function(
   dbPath = ".",
   models = list.dirs(path = dbPath, full.names = FALSE, recursive = FALSE),
-  example = FALSE,
   methodsFilter = NULL,
   obsNrFilter = NULL,
   truthNrFilter = NULL,
@@ -19,7 +18,7 @@ runEval <- function(
     message("MODEL: ", model)
     pt <- proc.time()
 
-    path <- DEEBpath::getPaths(dbPath, model, example=example)
+    path <- DEEBpath::getPaths(dbPath, model)
 
     methods <- list.dirs(path$esti, full.names = FALSE, recursive = FALSE)
     if (!is.null(methodsFilter)) methods <- intersect(methods, methodsFilter)
@@ -80,64 +79,59 @@ runEval <- function(
 #' @export
 runEvalTbl <- function(
     dbPath,
-    tbl, # use getUnevaled() to get a suitable tibble with columns c("truthNr", "obsNr", "taskNr", "method", "model", "example")
+    tbl, # use getUnevaled() to get a suitable tibble with columns c("truthNr", "obsNr", "taskNr", "method", "model")
     scoreFilter = NULL,
     createPlots = TRUE,
     verbose = FALSE
 ) {
   models <- tbl$model |> unique()
   methods <- tbl$method |> unique()
-  examples <- tbl$example |> unique()
 
   # TODO: remove code duplications with runEval()
 
-  for (example in examples) {
-    for (model in models) {
-      message("MODEL: ", model)
-      if (example) message("example")
-      pt <- proc.time()
-      path <- DEEBpath::getPaths(dbPath, model, example=example)
-      for (method in methods) {
-        cat(method)
-        ptMethod <- proc.time()
-        methodEstiPath <- file.path(path$esti, method)
-        if (!dir.exists(methodEstiPath)) next
-        meta <-
-          DEEBpath::getMetaGeneric(
-            c(path$truth, path$obs, methodEstiPath, path$task),
-            tagFileFilter = list(
-              c("truth", "obs", "task", "esti"),
-              c("task", "truth"),
-              "task"),
-            removeNa = TRUE)
-        meta <-
-          meta |>
-          dplyr::semi_join(
-            tbl |> dplyr::filter(
-              .data$method == .env$method,
-              .data$model == .env$model,
-              .data$example == .env$example),
-            by = c("truthNr", "obsNr", "taskNr"))
-        if (nrow(meta) == 0) next
-        evalMetaAndWriteToFile(
-          meta,
-          path$eval,
-          path$plots,
-          method,
-          createPlots = createPlots,
-          scoreFilter = scoreFilter,
-          verbose = verbose)
-        if (createPlots) createExtraPlots(path, method)
-        cat(" took ", format((proc.time()-ptMethod)[3]), "s\n", sep="")
-      }
-
-      writeDoc(
+  for (model in models) {
+    message("MODEL: ", model)
+    pt <- proc.time()
+    path <- DEEBpath::getPaths(dbPath, model)
+    for (method in methods) {
+      cat(method)
+      ptMethod <- proc.time()
+      methodEstiPath <- file.path(path$esti, method)
+      if (!dir.exists(methodEstiPath)) next
+      meta <-
+        DEEBpath::getMetaGeneric(
+          c(path$truth, path$obs, methodEstiPath, path$task),
+          tagFileFilter = list(
+            c("truth", "obs", "task", "esti"),
+            c("task", "truth"),
+            "task"),
+          removeNa = TRUE)
+      meta <-
+        meta |>
+        dplyr::semi_join(
+          tbl |> dplyr::filter(
+            .data$method == .env$method,
+            .data$model == .env$model),
+          by = c("truthNr", "obsNr", "taskNr"))
+      if (nrow(meta) == 0) next
+      evalMetaAndWriteToFile(
+        meta,
         path$eval,
-        "scores",
-        paths = DEEBpath::getScoreFiles(path$eval))
-
-      message(model, " took ", format((proc.time()-pt)[3]), "s")
+        path$plots,
+        method,
+        createPlots = createPlots,
+        scoreFilter = scoreFilter,
+        verbose = verbose)
+      if (createPlots) createExtraPlots(path, method)
+      cat(" took ", format((proc.time()-ptMethod)[3]), "s\n", sep="")
     }
+
+    writeDoc(
+      path$eval,
+      "scores",
+      paths = DEEBpath::getScoreFiles(path$eval))
+
+    message(model, " took ", format((proc.time()-pt)[3]), "s")
   }
 }
 
