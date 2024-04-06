b51d799fd290394c0c2e6c398c15ec8b6da5575e
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-04-06 15:20:36 +0200

    moved remove empty

--- R/generateBestHyperCube.R
+++ R/generateBestHyperCube.R
@@ -38,9 +38,13 @@ generateBestHyperCube <- function(dbPath, timeInMinutes = 60) {
     }
 
     optsProto <- ConfigOpts::readOptsBare(filePath)
+    if (!ConfigOpts::hasGenerativeExpands(optsProto)) {
+      cat(info$methodBase, "does not have generative elemets. Skipping.\n")
+      next
+    }
     optsProto$list <- optsProto$list[1]
     optsBest <- ConfigOpts::readOpts(info$filePath)
-    optsProto$list[[1]] <- replaceExpandValues(optsProto$list[[1]], optsBest)
+    optsProto$list[[1]] <- ConfigOpts::replaceExpandValues(optsProto$list[[1]], optsBest)
 
     outFile <- getFreeFilePath(bestHyperCubePaths$dirPath, info$methodBase, "json")
 
@@ -159,45 +163,3 @@ getTargetInfo <- function(dbPath, model) {
 }
 
 
-replaceExpandValues <- function(proto, replacement) {
-  if (inherits(proto, "expansion")) {
-    if (!"generate" %in% names(proto)) return(proto)
-    proto$values <- generateExpansionValues(replacement, proto$generate)
-    return(proto)
-  }
-  if (is.list(proto)) {
-    stopifnot(is.list(replacement))
-    if (!is.null(names(proto))) {
-      stopifnot(all(names(proto) %in% names(replacement)))
-      for (nm in names(proto)) {
-        proto[[nm]] <- replaceExpandValues(proto[[nm]], replacement[[nm]])
-      }
-    } else {
-      stopifnot(length(proto) == length(replacement))
-      for (i in seq_along(proto)) {
-        proto[[i]] <- replaceExpandValues(proto[[i]], replacement[[i]])
-      }
-    }
-    return(proto)
-  }
-  return(proto)
-}
-
-
-generateExpansionValues <- function(value, generateInfo) {
-  out <- switch(
-    generateInfo$kind,
-    multiply = generateExpansionValuesMultiply(value, generateInfo$value),
-    add = generateExpansionValuesAdd(value, generateInfo$value),
-    stop("Unknown generate kind ", kind))
-  out <- out[out >= generateInfo$min & out <= generateInfo$max]
-  return(out)
-}
-
-generateExpansionValuesMultiply <- function(value, factor) {
-  c(value / factor, value, value * factor)
-}
-
-generateExpansionValuesAdd <- function(value, summand) {
-  c(value - summand, value, value + summand)
-}
