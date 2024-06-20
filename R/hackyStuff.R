eb530dc5d67083b6ccf3464cf338def5bd57290f
Christof Schötz <math@christof-schoetz.de>
2024-06-20 16:45:36 +0200

    hacks

--- R/hackyStuff.R
+++ R/hackyStuff.R
@@ -25,3 +25,45 @@ hackCreateSmapeSeries <- function(dbPath) {
     }
   }
 }
+
+
+#' @export
+hackCreateSmapeSeriesPlot <- function(dbPath) {
+  models <- DEEBpath::getModels(dbPath)
+  data <-
+    lapply(models, \(model) {
+      cat("Model:", model, "\n")
+      paths <- DEEBpath::getPaths(dbPath, model)
+      methods <- list.dirs(paths$esti, recursive=FALSE)
+      lapply(methods, \(method) {
+        smapePath <- file.path(method, "sMAPE_truth0001obs0001task01esti.csv")
+        DEEBtrajs::readTrajs(smapePath) |>
+          rename(smape = state) |>
+          mutate(smape = as.vector(smape)) |>
+          mutate(method = basename(method))
+      }) |>
+        bind_rows() |>
+        mutate(model = model)
+    }) |>
+      bind_rows()
+  data |>
+    ggplot(aes(x = time, y = smape, color = method)) +
+    geom_line() +
+    facet_wrap(vars(model), scales="free")
+  le <- read_csv(file.path(dbPath, "_info/LyapunovExponents.csv"))
+  data |>
+    left_join(le, join_by(model)) |>
+    group_by(model, method) |>
+    mutate(time = lyapunovExponent*(time - time[1])) |>
+    summarize(max(time))
+  data |>
+    left_join(le, join_by(model)) |>
+    group_by(model, method) |>
+    #mutate(time = lyapunovExponent*(time - time[1])) |>
+    summarize(max(time))
+  data |>
+    summarise(smape = smape[10], .by = c("model", "method")) |>
+    ggplot(aes(x = method, y = smape, color = model)) + geom_point()
+}
+
+
